890a47ec301595117d52de7f2ef647a1b043b507
RaulJiminian <raul.e.jiminian@gmail.com>
2025-09-05 16:45:32 -0400

    final

---
+++ README.md
@@ -0,0 +1,21 @@
+# Skyrockit
+
+- Clone the repo
+- Remove git remote connection:
+
+```
+rm -rf .git
+```
+
+- Add a .env file to your application and add the following secret keys to your application.
+
+```
+MONGODB_URI=
+SESSION_SECRET=
+```
+
+- Install dependencies
+
+```
+npm i
+```
--- controllers/applications.js
+++ controllers/applications.js
@@ -3,10 +3,31 @@ const router = express.Router();
 
 const User = require("../models/user.js");
 
-// Write all routes/controller logic here
 router.get("/", async (req, res) => {
   try {
-    res.render("applications/index.ejs");
+    const currentUser = await User.findById(req.session.user._id);
+
+    res.render("applications/index.ejs", {
+      applications: currentUser.applications,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
+router.get("/new", (req, res) => {
+  res.render("applications/new.ejs");
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.applications.push(req.body);
+
+    await currentUser.save();
+
+    res.redirect("/applications");
   } catch (error) {
     console.log(error);
     res.redirect("/");
