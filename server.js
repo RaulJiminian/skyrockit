a04f6c3f28627a0eaed30fc884630b7f54cac4d3
RaulJiminian <raul.e.jiminian@gmail.com>
2025-09-05 15:53:22 -0400

    add navbar partial

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ const db = require("./db/connection.js");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 const session = require("express-session");
+const isSignedIn = require("./middleware/is-signed-in.js");
+const passUserToView = require("./middleware/pass-user-to-view.js");
 
 const authController = require("./controllers/auth.js");
+const applicationsController = require("./controllers/applications.js");
 
 const port = process.env.PORT ? process.env.PORT : "3000";
 
@@ -23,15 +26,26 @@ app.use(
   })
 );
 
+app.use(passUserToView);
+
 app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    user: req.session.user,
-  });
+  // Check if the user is signed in
+  if (req.session.user) {
+    // Redirect signed-in users to their applications index
+    res.redirect("/applications");
+  } else {
+    // Show the homepage for users who are not signed in
+    res.render("index.ejs");
+  }
 });
 
 app.use("/auth", authController);
+app.use(isSignedIn);
+
+app.use("/applications", applicationsController);
 
 db.on("connected", () => {
+  console.clear();
   console.log(`Connected to MongoDB.`);
   app.listen(port, () => {
     console.log(`The express app is ready on port ${port}!`);
