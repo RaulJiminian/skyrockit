e80781b887c6e1ba3a242017118e99a26faa4f93
RaulJiminian <raul.e.jiminian@gmail.com>
2025-09-08 11:23:53 -0400

    ready for styling

--- controllers/applications.js
+++ controllers/applications.js
@@ -20,6 +20,22 @@ router.get("/new", (req, res) => {
   res.render("applications/new.ejs");
 });
 
+router.get("/:applicationId", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const applicationData = currentUser.applications.id(
+      req.params.applicationId
+    );
+
+    res.render("applications/show.ejs", {
+      application: applicationData,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
 router.post("/", async (req, res) => {
   try {
     const currentUser = await User.findById(req.session.user._id);
@@ -32,6 +48,44 @@ router.post("/", async (req, res) => {
     console.log(error);
     res.redirect("/");
   }
+});
+
+router.delete("/:applicationId", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.applications.id(req.params.applicationId).deleteOne();
+
+    await currentUser.save();
+
+    res.redirect("/applications");
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
+router.get("/:applicationId/edit", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const application = currentUser.applications.id(req.params.applicationId);
+    res.render("applications/edit.ejs", {
+      application: application,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
+router.put("/:applicationId", async (req, res) => {
+  const currentUser = await User.findById(req.session.user._id);
+  const application = currentUser.applications.id(req.params.applicationId);
+
+  application.set(req.body);
+
+  await currentUser.save();
+
+  res.redirect(`/applications/${req.params.applicationId}`);
 });
 
 module.exports = router;
